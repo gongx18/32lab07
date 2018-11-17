6b6cdd72122ad12b67a7081373c707038075a8b8
Xi Gong <xi@ucsb.edu>
2018-11-16 23:19:41 -0800

    Version 2.0

---
+++ SimpleList.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "SimpleList.h"
+
+
+template<typename T>
+SimpleList<T>::SimpleList(){
+    elements = new T[CAPACITY]; 
+    numElements = 0;
+}
+
+template<typename T>
+SimpleList<T>::~SimpleList(){
+    delete[] elements; //memorize this!!! 
+}
+
+template<typename T>
+T SimpleList<T>::at(int index) const throw (InvalidIndexException){
+    if(index < 0 || index > numElements -1){
+	//how to throw exceptions?
+	InvalidIndexException i; 
+	throw i; 
+    }
+    else{
+	return elements[index]; 
+    }
+}
+
+template<typename T>
+T SimpleList<T>::first() const throw (EmptyListException){
+     if(numElements == 0){
+	EmptyListException excpt; 
+	throw excpt;
+     }
+     else{
+	return elements[0];
+     }
+} 
+
+
+
+template<typename T>
+bool SimpleList<T>::empty() const{
+    return numElements == 0; 
+} 
+
+template<typename T>
+T SimpleList<T>::last() const throw (EmptyListException){
+    if(numElements == 0){
+	EmptyListException excpt;
+	throw excpt;
+    }
+    else{
+	return elements[numElements - 1];
+    }
+}
+
+template<typename T>
+int SimpleList<T>::getNumElements() const{
+    return numElements;
+}
+
+template<typename T>
+void SimpleList<T>::insert(T item) throw (FullListException){
+    if(numElements ==CAPACITY){
+        FullListException full;
+        throw full;
+    }else{
+        elements[numElements] = item;
+        numElements++;
+    }        
+}
+
+template<typename T>
+void SimpleList<T>::remove(int index) throw (InvalidIndexException, EmptyListException){
+    if(index >= numElements || index < 0){
+	InvalidIndexException ii;
+	throw ii; 
+    }else if(numElements == 0){
+	EmptyListException e;
+	throw e; 
+    }else{
+	for(int i = index; i < index; i++){
+	    elements[i] = elements[i + 1]; 
+	}
+    }
+}
